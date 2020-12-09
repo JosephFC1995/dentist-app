e79e44a73c0f17e382cde5bad0e7112b1e8aada0
Joseph <josefc9512@gmail.com>
2020-12-08 22:24:22 -0500

    update now.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
             },
         },
     },
