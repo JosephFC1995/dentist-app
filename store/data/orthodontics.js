5409addf9b265c218cf7a26937acb5b323b5ab0e
Joseph <j.flores@comunal.co>
2021-02-28 21:17:10 -0500

    Feat logic in Padiatry Dent and ORthodontic forms.

---
+++ store/data/orthodontics.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const state = () => ({
+    dates: [],
+    loading: false,
+})
+
+export const mutations = {
+    SET_DATES_ORTHODONTIC(state, payload) {
+        state.dates = payload
+    },
+    SET_LOADING(state, payload) {
+        state.loading = payload
+    },
+}
+
+export const actions = {
+    async GET_DATES_ORTHODONTIC({ commit, state }, options) {
+        const { data } = await this.$axios.$get('/orthodontic_patient', { params: options })
+        commit('SET_LOADING', false)
+        commit('SET_DATES_ORTHODONTIC', data)
+    },
+    async GET_ORTHODONTIC_BY_PATIENT({ commit, state }, id, options = {}) {
+        const { data } = await this.$axios.$get(`/orthodontic_patient/${id}`, { params: options })
+        return data
+    },
+    async CHANGE_LOADING({ commit, state }, loading) {
+        commit('SET_LOADING', loading)
+    },
+}
+
+export const getters = {
+    getDates: (state) => {
+        return _.orderBy(state.dates, ['date'], ['desc'])
+    },
+    getLoading: (state) => {
+        return state.loading
+    },
+}
