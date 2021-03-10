7bfa8a9573cbc156d86668ae64b9804b4cf34648
Joseph <j.flores@comunal.co>
2021-03-10 11:00:59 -0500

    maps

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 require('dotenv').config()
-
+console.log(process.env.KEY_GOOGLE_MAPS)
 Vue.use(VueGoogleMaps, {
     load: {
         key: process.env.KEY_GOOGLE_MAPS,
